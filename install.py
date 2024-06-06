fb5b102d52e75999205b18d3d35e319fbccc9f35
Masoud Gb <87688187+masoudgb@users.noreply.github.com>
2024-06-07 03:01:39 +0330

    Update install.py

--- install.py
+++ install.py
@@ -75,7 +75,7 @@ def prepare(rType="MAIN"):
         printc("Install MariaDB 11.5 repository")
         os.system("apt-get install -y software-properties-common > /dev/null")
         os.system("apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8 >/dev/null 2>&1")
-        os.system("add-apt-repository 'deb [arch=amd64,arm64,ppc64el,s390x] http://mirror.lstn.net/mariadb/repo/11.5/ubuntu noble main'  > /dev/null")
+        os.system("add-apt-repository 'deb [arch=amd64,arm64,ppc64el,s390x] http://mirror.lstn.net/mariadb/repo/11.5/ubuntu noble main' -y > /dev/null")
         os.system("apt-get update > /dev/null")
     for rPackage in rPackages:
         printc("Installing %s" % rPackage)
