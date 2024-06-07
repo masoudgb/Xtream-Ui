90f68a1a39294f83d26fcb0bea9f4146430950ed
Masoud Gb <87688187+masoudgb@users.noreply.github.com>
2024-06-07 07:43:33 +0330

    Update install.py

--- install.py
+++ install.py
@@ -86,7 +86,7 @@ def prepare(rType="MAIN"):
     import subprocess
     subprocess.run(["systemctl", "daemon-reload"])
     try:
-        subprocess.check_output("getent passwd xtreamcodes > /dev/null".split())
+    subprocess.check_output("getent passwd xtreamcodes > /dev/null".split())
     except:
         # Create User
         printc("Creating user xtreamcodes")
