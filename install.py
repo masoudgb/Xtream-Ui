943765d29a70fb70463792527ea4c5705c30a262
Masoud Gb <87688187+masoudgb@users.noreply.github.com>
2024-06-09 16:43:41 +0330

    Update install.py

--- install.py
+++ install.py
@@ -71,15 +71,12 @@ def prepare(rType="MAIN"):
     printc("Updating Operating System")
     os.system("apt-get update > /dev/null")
     os.system("apt-get -y full-upgrade > /dev/null")
-    import os
-    import pyautogui
 
-if rType == "MAIN":
+    if rType == "MAIN":
     printc("Install MariaDB 11.5 repository")
     os.system("apt-get install -y software-properties-common")
     os.system("apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8")
     os.system("add-apt-repository 'deb [arch=amd64,arm64,ppc64el,s390x] https://mirrors.xtom.com/mariadb/repo/11.5/ubuntu noble main' -y")
-    pyautogui.press('enter')  # ارسال کلید Enter
     os.system("apt-get update > /dev/null")
     for rPackage in rPackages:
         printc("Installing %s" % rPackage)
