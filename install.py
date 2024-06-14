9994e48b4c7d8735a404f6642d69ae3909587832
Masoud Gb <87688187+masoudgb@users.noreply.github.com>
2024-06-14 19:48:01 +0330

    Update install.py

--- install.py
+++ install.py
@@ -21,7 +21,7 @@ class col:
     BLACK = '\033[30m'
     RED = '\033[31m'
     GREEN = '\033[32m'
-    YELLOW = '\033[33m' # orange on some systems
+    YELLOW = '\033[33m'
     BLUE = '\033[34m'
     MAGENTA = '\033[35m'
     CYAN = '\033[36m'
